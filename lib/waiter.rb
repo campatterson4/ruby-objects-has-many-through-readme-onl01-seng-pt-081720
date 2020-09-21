d3a1f420328dc37232711fec32bae38df98333e3
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-21 02:20:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,7 @@
 class Waiter
 
-  attr_accessor :name, :yrs_exp
-
-  @@all = []
+attr_accessor :name, :yrs_exp
+@@all = []
 
   def initialize(name, yrs_exp)
     @name = name
@@ -14,23 +13,16 @@ class Waiter
     @@all
   end
 
-  # OBJECT RELATIONSHIPS - #new_meal, #meals, #best_tippers
-
-  # Initialize a meal using the current Waiter instance, 
-    # a provided Customer instance and a total and tip
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
 
-  # Returns an Array of Meal instances associated with this waiter
   def meals
     Meal.all.select do |meal|
       meal.waiter == self
     end
   end
 
-  # Returns Customer instance associated with the meal that 
-    # received the largest tip
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
@@ -38,5 +30,4 @@ class Waiter
 
     best_tipped_meal.customer
   end
-
-end
+  end
